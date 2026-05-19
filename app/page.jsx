ea969c28726dc676f0db97c8978a9308b99ee7ff
Alice Visintin <alicevisintin@outlook.com>
2026-05-19 13:34:12 +0000

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,32 @@
+import { createClient } from "@/prismicio";
+import * as prismic from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
+import { components } from "@/slices";
+
+const queryHomepage = () => {
+  const client = createClient();
+  return client.getSingle("homepage");
+};
+
+export async function generateMetadata() {
+  const page = await queryHomepage();
+
+  return {
+    openGraph: {
+      title: page.data.meta_title,
+      description: prismic.asText(page.data.meta_description),
+      images: prismic.asImageSrc(page.data.meta_image),
+    },
+  };
+}
+
+export default async function Home() {
+  const page = await queryHomepage();
+
+  console.log(page.data.slices);
+  return (
+    <main>
+      <SliceZone slices={page.data.slices} components={components} />
+    </main>
+  );
+}
---
+++ slices/TextSlice/index.js
@@ -0,0 +1,32 @@
+import { PrismicRichText } from "@prismicio/react";
+import Button from "@/components/Button";
+
+/**
+ * @typedef {import("@prismicio/client").Content.TextSliceSlice} TextSliceSlice
+ * @typedef {import("@prismicio/react").SliceComponentProps<TextSliceSlice>} TextSliceProps
+ * @type {import("react").FC<TextSliceProps>}
+ */
+const TextSlice = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div
+        className={`py-24 gap-16 inline-flex flex-col items-center w-screen bg-greenGrey ${slice.primary.text_align || "text-center"}`}
+      >
+        <div className="container gap-6 flex flex-col items-center">
+          <PrismicRichText field={slice.primary.text_field} />
+          {slice.variation === "callToActionButton" && (
+            <Button
+              link={slice.primary.button_link}
+              label={slice.primary.button_label}
+            />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TextSlice;
